60468170edac2c63f71f8990ad602b7d46dfb120
Steve Quirke <squirke@thoughtworks.com>
2019-03-18 11:56:28 +0000

    :loud_sound: (send_log.py) Modify message logged at start of the process

--- user_scripts/send_log.py
+++ user_scripts/send_log.py
@@ -13,7 +13,7 @@ def my_serial():
 
 def main():
     serial_number = my_serial()[0]
-    logtoSumo(serial_number, "InstallApplications is go!")
+    logtoSumo(serial_number, "SimpleMDM install enterprise application command has triggered successfully.")
 
 
 if __name__ == "__main__":
